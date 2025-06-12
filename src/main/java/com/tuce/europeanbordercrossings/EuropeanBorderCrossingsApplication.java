78e2f2104979fb278eb09d8bcfa2b4ae8215c8c5
Tuce Sari Tekkalmaz <tuce.saritekkalmaz@cgi.com>
2025-06-12 22:35:15 +0200

    first commit

---
+++ src/main/java/com/tuce/europeanbordercrossings/EuropeanBorderCrossingsApplication.java
@@ -0,0 +1,13 @@
+package com.tuce.europeanbordercrossings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuropeanBorderCrossingsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EuropeanBorderCrossingsApplication.class, args);
+  }
+
+}
